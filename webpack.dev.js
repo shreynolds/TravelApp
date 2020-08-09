9cfd203619c4e30390e42a2e0f69df1faf4877d9
Sophie Reynolds <shreynolds101@gmail.com>
2020-08-09 11:04:53 -0700

    Added output and scss test

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,12 +8,23 @@ module.exports = {
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    devServer: {
+        port: 8000
+      },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
